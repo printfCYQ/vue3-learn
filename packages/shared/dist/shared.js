9a6c53aef646f6d9e2628beb11cc8240f2dc2967
printfCYQ <459987870@qq.com>
2025-01-14 21:22:58 +0800

    3.搭建 esbuild 开发环境

---
+++ packages/shared/dist/shared.js
@@ -0,0 +1,8 @@
+// packages/shared/src/index.ts
+function isObject(value) {
+  return typeof value === "object" && value !== null;
+}
+export {
+  isObject
+};
+//# sourceMappingURL=shared.js.map
--- script/dev.js
+++ script/dev.js
@@ -1,19 +1,33 @@
+import esbuild from 'esbuild';
 import minimist from 'minimist';
 import { createRequire } from 'module';
 import { dirname, resolve } from 'path';
-import { fileURLPath } from 'url';
+import { fileURLToPath } from 'url';
+// node dev.js (打包哪个项目 -f 打包什么格式) == argv.slice(2)
 
-// node dev.js (打包哪个项目 -f 打包什么格式) == args.slice(2)
-
-const args = minimist(process.args.slice(2))
-const __filename = fileURLPath(import.meta.url) // 获取文件的绝对路径 file: -> /usr
+const args = minimist(process.argv.slice(2))
+const __filename = fileURLToPath(import.meta.url) // 获取文件的绝对路径 file: -> /usr
 const __dirname = dirname(__filename)
 const require = createRequire(import.meta.url)
 
-const target = args[0]._[0] || 'reactivity' // 打包哪个项目
+const target = args._[0] || 'reactivity' // 打包哪个项目
 const format = args.f || 'iife' // 打包什么格式（模块化 cjs esm ... ）
 
-console.log(target, format);
+// console.log(target, format);
 
 // 入口文件
 const entry = resolve(__dirname, `../packages/${target}/src/index.ts`)
+const pgk = require(`../packages/${target}/package.json`)
+
+esbuild.context({
+    entryPoints: [entry],
+    outfile: resolve(__dirname, `../packages/${target}/dist/${target}.js`),
+    bundle: true,
+    platform: "browser",
+    sourcemap: true,
+    format, //（cjs esm iife） iife 必须给 name
+    globalName: pgk.buildOptions?.name
+}).then((ctx) => {
+    console.log("🥳🥳🥳 start dev");
+    return ctx.watch() // 监听入口文件，持续打包
+})
